700ee4615f327594bf95f8ab1d55627266ab128b
EQuimper <quimperemanuel@gmail.com>
2016-05-24 15:49:10 -0600

    Finish Testing

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,35 +1,45 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-const $ = _$(window);
+// Set up testing environment to run like a browser in the command line
 
-chaiJquery(chai, chai.util, $);
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>'); // Fake the DOM
+global.window = global.document.defaultView; // Fake the BROWSER
+const $ = jquery(global.window); // Hook jQuery to the fake DOM
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-	const componentInstance =  TestUtils.renderIntoDocument(
+// Build 'renderComponent' helper that should render a given react class.
+
+function renderComponent(ComponentClass, props, state) {
+	const componentInstance = TestUtils.renderIntoDocument(
 		<Provider store={createStore(reducers, state)}>
-			<ComponentClass {...props} />
+			<ComponentClass {...props}/>
 		</Provider>
 	);
 
-	return $(ReactDOM.findDOMNode(componentInstance));
+	return $(ReactDOM.findDOMNode(componentInstance)); // Produces HTML
 }
 
-$.fn.simulate = function(eventName, value) {
+// Build helper for simulating event.
+
+$.fn.simulate = function (eName, value) {
 	if (value) {
 		this.val(value);
 	}
-	TestUtils.Simulate[eventName](this[0]);
+	TestUtils.Simulate[eName](this[0]);
 };
 
-export {renderComponent, expect};
+// Set up Chai-jQuery
+
+chaiJquery(chai, chai.util, $);
+
+// Export
+
+export { renderComponent, expect };
